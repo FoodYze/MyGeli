26ace6881f9eb2f85a0f1773aa62638cf3e99004
Heitor Vitti Partezani <heitorhvp@outlook.com>
2025-09-18 10:09:28 -0300

    Update and rename foodyze.sql to mygeli.sql

--- App/mygeli.sql
+++ App/mygeli.sql
@@ -18,8 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `foodyze`
+-- Cria o banco de dados 'mygeli' se ele não existir
 --
+CREATE DATABASE IF NOT EXISTS `mygeli` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `mygeli`;
 
 -- --------------------------------------------------------
 
